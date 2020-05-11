499c82abb7a09d0088c26da16210698883f4ae16
Eddie Rosas <eddie_rosas@outlook.com>
2020-05-11 00:21:10 -0700

    enhance skeleton with database and create basic user model/routes

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
-const port =  process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const passport = require("passport");
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Hi it me. it werk"))
+mongoose
+    .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users)
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-// Uncomment below when we add in our MongoDB @eddie
-
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log("Connected to MongoDB successfully"))
-//   .catch((err) => console.log(err));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
