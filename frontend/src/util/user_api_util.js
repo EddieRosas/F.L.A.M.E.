b4fe60a8f3f016989481d96ed5a4a9fb1f46f12e
Shaphen <swpburn@gmail.com>
2020-05-14 10:15:40 -0700

    Add user actions for fi numbers

--- frontend/src/util/user_api_util.js
+++ frontend/src/util/user_api_util.js
@@ -4,10 +4,10 @@ export const fetchFiData = (userId) => {
   return axios.patch(`/api/user/${userId}`);
 };
 
-export const updateFiNum = (user, fiNum) => {
-  return axios.patch(`/api/user/${user._id}`, fiNum);
+export const updateFiNum = (user, fireNum) => {
+  return axios.patch(`/api/user/${user._id}`, fireNum);
 };
 
-export const updateNumYears = (user, numYears) => {
-  return axios.patch(`/api/user/${user._id}`, numYears);
+export const updateYearsToFI = (user, yearsToFI) => {
+  return axios.patch(`/api/user/${user._id}`, yearsToFI);
 };
