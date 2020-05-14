d6ffb3dcee81668719c33b7182cfdb14b3c27707
bcwan <bcwan8@gmail.com>
2020-05-14 11:39:48 -0700

    Add Chefs CSS changes

--- frontend/src/components/budget/budget_list.jsx
+++ frontend/src/components/budget/budget_list.jsx
@@ -4,28 +4,27 @@ import "./budget.css";
 
 class BudgetList extends React.Component {
   componentDidMount() {
+    debugger;
     this.props.fetchEntries();
+    debugger;
   }
 
   componentDidUpdate(prevProps, prevState) {
 
-    // we do === prevProps and this.props because when we first render the budget page, our entries
-    // in render destructuring in props is empty array, and that componentWillMount fetchEntries
-    // will help us refresh our data. Our props entries has things now. ComponentDidUpdate will be called 
-    // afterwards, and we know our prevProps for entry is [] and now this.props.entries has things. We don't 
-    // need to call fetchEntries again.
+    debugger;
 
-    // this.props will not update until fetchEntries is called, therefore if prevProps and this.props is same,
-    // we have to double check
-    if ((JSON.stringify(prevProps.entries.length) === JSON.stringify(this.props.entries.length)) 
-        && !(prevProps.entries.length === 0 && this.props.entries.length === 0)){
+    if (JSON.stringify(prevProps.entries.length) !== JSON.stringify(this.props.entries.length)) {
+      debugger;
       this.props.fetchEntries();
+
     }
   }
   
   render() {
     const { deleteEntry, updateEntry, entries } = this.props;
+    debugger;
     if(!entries.length) {
+      debugger;
       return null;
     }
     return (
