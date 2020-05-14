945a516b3226ea996c8c8bc15fdfeec71729f67d
Shaphen <swpburn@gmail.com>
2020-05-14 11:57:18 -0700

    Minor styling to budget form

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,7 +2,6 @@ import { RECEIVE_FI_DATA } from "../actions/user_actions";
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  let nextState = Object.assign({}, state);
 
   switch (action.type) {
     case RECEIVE_FI_DATA:
--- frontend/src/util/user_api_util.js
+++ frontend/src/util/user_api_util.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const fetchFiData = (userId) => {
-  return axios.patch(`/api/user/${userId}`);
+  return axios.patch(`/api/users/${userId}`);
 };
 
 export const updateFiNum = (user, fireNum) => {
-  return axios.patch(`/api/user/${user._id}`, fireNum);
+  return axios.patch(`/api/users/${user._id}`, fireNum);
 };
 
 export const updateYearsToFI = (user, yearsToFI) => {
-  return axios.patch(`/api/user/${user._id}`, yearsToFI);
+  return axios.patch(`/api/users/${user._id}`, yearsToFI);
 };
