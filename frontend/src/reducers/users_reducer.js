baa869ce92dd6401b69082e362ca0f97203fbaf6
Shaphen <swpburn@gmail.com>
2020-05-14 10:41:12 -0700

    Fix bug clicking anywhere on edit and delete box budget form

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,7 +2,7 @@ import { RECEIVE_FI_DATA } from "../actions/user_actions";
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  nextState = Object.assign({}, state);
+  let nextState = Object.assign({}, state);
 
   switch (action.type) {
     case RECEIVE_FI_DATA:
