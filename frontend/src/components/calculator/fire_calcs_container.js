047ba61b19344df89deb5a0125b55590ce50dc2d
Eddie Rosas <eddie_rosas@outlook.com>
2020-05-14 19:17:50 -0700

    create algorithm for calculating data

--- frontend/src/components/calculator/fire_calcs_container.js
+++ frontend/src/components/calculator/fire_calcs_container.js
@@ -4,32 +4,41 @@ import Calculators from './fire_calcs';
 
 
 const mSTP = (state) => {
+    
     let entries = Object.values(state.budgetTable)
+
+
+    let monthsHash = {
+        1 : { month: "Jan", expenses: 0, income: 0 },
+        2 : { month: "Feb", expenses: 0, income: 0 },
+        3 : { month: "March", expenses: 0, income: 0 },
+        4 : { month: "Apr", expenses: 0, income: 0 },
+        5 : { month: "May", expenses: 0, income: 0 },
+        6 : { month: "June", expenses: 0, income: 0 },
+        7 : { month: "July", expenses: 0, income: 0 },
+        8 : { month: "Aug", expenses: 0, income: 0 },
+        9 : { month: "Sept", expenses: 0, income: 0 },
+        10 : { month: "Oct", expenses: 0, income: 0 },
+        11 : { month: "Nov", expenses: 0, income: 0 },
+        12 : { month: "Dec", expenses: 0, income: 0 }
+    }
+
+    entries.forEach(entry => {
+        let monthNum = (new Date(entry.date).getMonth() + 1)
+        if (monthsHash[monthNum]) {
+            if (entry.incomeOrDebt === false) {
+                monthsHash[monthNum].expenses += Number(entry.amount.$numberDecimal)
+            } else {
+                monthsHash[monthNum].income += Number(entry.amount.$numberDecimal)
+            }
+        }
+    });
     
-    // let data = []
+    let data = Object.values(monthsHash);
 
-
-    // // iterate thru each object, check the month, and add expense or debt to 
-    //     // corresponding category
-    // let jan = 
-        
-    //     entries.forEach(entry => )
-
-    // let feb
-    // let mar
-    // let apr
-    // let may
-    // let jun
-    // let jul
-    // let aug
-    // let sept
-    // let oct
-    // let nov
-    // let dec
-
-    
     return ( {
-        entries: entries
+        entries: entries,
+        data: data
     }
     )
 }
