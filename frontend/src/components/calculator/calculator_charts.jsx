93d28c6910d1b732382cd3f15fd17c85abf431f9
Shaphen <swpburn@gmail.com>
2020-05-14 21:19:04 -0700

    Adjust sizing calc chart

--- frontend/src/components/calculator/calculator_charts.jsx
+++ frontend/src/components/calculator/calculator_charts.jsx
@@ -8,6 +8,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer
 } from "recharts";
 import "./calc_chart.css";
 
@@ -16,20 +17,22 @@ class CalculatorCharts extends React.Component {
   render() {
     return (
       <div className="fi-chart-box">
-        <BarChart
-          className="calc-bar-chart"
-          width={700}
-          height={500}
-          data={this.props.data}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="income" fill="rgb(40, 200, 50)" />
-          <Bar dataKey="expenses" fill="rgb(230, 60, 40)" />
-        </BarChart>
+        <ResponsiveContainer width="55%" height="65%">
+          <BarChart
+            className="calc-bar-chart"
+            // width={700}
+            // height={500}
+            data={this.props.data}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="income" fill="rgb(40, 200, 50)" />
+            <Bar dataKey="expenses" fill="rgb(230, 60, 40)" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     );
   }
