2253fd93782a88383910a1ca93bfb97e67adb294
Shaphen <swpburn@gmail.com>
2020-05-14 20:58:10 -0700

    Fix overflow bug main budget page

--- frontend/src/components/calculator/calculator_charts.jsx
+++ frontend/src/components/calculator/calculator_charts.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import "./calculator.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import "./calc_chart.css";
 
 class CalculatorCharts extends React.Component {
 
   render() {
     return (
       <div className="fi-chart-box">
-        <p>-insert edgy neon green chart-</p>
+        <BarChart
+          className="calc-bar-chart"
+          width={700}
+          height={500}
+          data={this.props.data}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="income" fill="rgb(40, 200, 50)" />
+          <Bar dataKey="expenses" fill="rgb(230, 60, 40)" />
+        </BarChart>
       </div>
     );
   }
