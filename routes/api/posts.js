7b592da0caaea5c5e1fd384cd763526d1790e046
Shaphen <swpburn@gmail.com>
2020-06-09 15:15:11 -0700

    Add get all post logic

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,3 +4,14 @@ const passport = require('passport');
 
 const Post = require('../../models/Post');
 // const ValidatePost = require('../../validation/post');
+
+router.get("/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.all
+      .sort({ date: -1 })
+      .then(posts => res.json(posts))
+      .catch(err => res.status(404).json({ noPostsFound: "No Posts were found :c" }))
+  }
+)
+
