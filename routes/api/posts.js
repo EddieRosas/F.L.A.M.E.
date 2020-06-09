a463697a1d179122d014fae7fde2d87bf9261002
Shaphen <swpburn@gmail.com>
2020-06-09 14:51:38 -0700

    Add empty post validation file

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
 const Post = require('../../models/Post');
+// const ValidatePost = require('../../validation/post');
