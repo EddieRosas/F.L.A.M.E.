de22be7c40f836ea73289dd12cd536daee6424a6
bcwan <bcwan8@gmail.com>
2020-06-10 23:44:52 -0700

    Set skeleton for post index

--- frontend/src/components/post/post_index/all_post_index.jsx
+++ frontend/src/components/post/post_index/all_post_index.jsx
@@ -4,7 +4,24 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Modal from "react-modal";
 
 class AllPostIndex extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      openModalForNewPost: false,
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  changeModalStatus(status) {
+    this.setState({ openModalForNewPost: status });
+  }
+
+  render() {
+    
+  }
 }
 
 export default AllPostIndex;
