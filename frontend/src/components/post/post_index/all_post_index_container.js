fb72860d7db9a5321c5bc6f6879c9dceafb989da
bcwan <bcwan8@gmail.com>
2020-06-10 22:54:31 -0700

    Create error reducers for posts and replies

--- frontend/src/components/post/post_index/all_post_index_container.js
+++ frontend/src/components/post/post_index/all_post_index_container.js
@@ -1,5 +1,11 @@
 import { connect } from 'react-redux';
 import AllPostIndex from './all_post_index.jsx';
 import {
-
+  fetchPosts,
+  fetchPost,
+  createPost
 } from '../../../actions/post_actions';
+
+const mSTP = (state) => ({
+  posts: Object.values
+});
