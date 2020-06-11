4931e509da3a061e63610ea97996c4fa9d0cd387
bcwan <bcwan8@gmail.com>
2020-06-10 23:42:38 -0700

    Import modals and bootstrap for AllPostIndex class

--- frontend/src/components/post/post_index/all_post_index.jsx
+++ frontend/src/components/post/post_index/all_post_index.jsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-modal";
+
+class AllPostIndex extends React.Component {
+  
+}
+
+export default AllPostIndex;
