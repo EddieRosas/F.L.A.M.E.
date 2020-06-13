ef96cf70dbbe55dc4bff9228a285b99291e83311
bcwan <bcwan8@gmail.com>
2020-06-12 18:27:21 -0700

    Clean code inside post reducers

--- frontend/src/reducers/posts_reducer.js
+++ frontend/src/reducers/posts_reducer.js
@@ -1,24 +1,25 @@
-import { RECEIVE_POSTS, RECEIVE_POST, REMOVE_POST,RECEIVE_POST_ERRORS,CLEAR_POST_ERRORS } from "../actions/post_actions";
+import { RECEIVE_POSTS, RECEIVE_POST, REMOVE_POST } from "../actions/post_actions";
+import { RECEIVE_USER_LOGOUT } from "../actions/session_actions";
 
 const postsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
 
     switch (action.type) {
-        case RECEIVE_POSTS:
-            return Object.assign({}, nextState, action.posts);
-        case RECEIVE_POST:
-            return Object.assign({}, nextState, { [action.post.data._id]: action.post.data });
-        case REMOVE_POST:
-            delete nextState[action.postId];
-            return nextState;
-        case RECEIVE_POST_ERRORS:
-            return {};
-        case CLEAR_POST_ERRORS:
-            return {};
-
-        default:
-            return state;
+      case RECEIVE_POSTS:
+        return Object.assign({}, nextState, action.posts);
+      case RECEIVE_POST:
+        return Object.assign({}, nextState, {
+          [action.post.data._id]: action.post.data,
+        });
+      case REMOVE_POST:
+        delete nextState[action.postId];
+        return nextState;
+      case RECEIVE_USER_LOGOUT:
+        return {};
+        
+      default:
+        return state;
     }
 }
 
