cec3ccd91a5a7f06040aea551e0a89fd433e9391
bcwan <bcwan8@gmail.com>
2020-06-13 23:49:31 -0700

    Fix navbar button setup after intergrading bootstrap

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,9 +2,10 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./reset.css";
 import "./app.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import MainPage from "./main/main_page.jsx";
 import LoginFormContainer from "./session/login_form_container";
