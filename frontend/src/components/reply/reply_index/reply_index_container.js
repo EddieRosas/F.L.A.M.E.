52b167f4de1af378277240968de712311859d376
bcwan <bcwan8@gmail.com>
2020-06-19 13:55:36 -0700

    Import actions and react to reply index container

---
+++ frontend/src/components/reply/reply_index/reply_index_container.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+import ReplyIndex from './reply_index';
+import {
+  fetchReplies,
+  fetchReply,
+  createReply,
+  deleteReply,
+  updateReply
+} from '../../../actions/reply_actions';
+
