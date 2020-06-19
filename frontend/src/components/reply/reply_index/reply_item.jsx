a508822ca868db433bcc49e23a117310d02be1c6
bcwan <bcwan8@gmail.com>
2020-06-19 15:52:46 -0700

    Display reply item description

--- frontend/src/components/reply/reply_index/reply_item.jsx
+++ frontend/src/components/reply/reply_index/reply_item.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { ListGroupItem } from 'react-bootstrap';
 
-const ReplyItem = () => {
-  
+const ReplyItem = ({ reply, deleteReply, updateReply, currentUser, postId }) => {
+  return (
+    <ListGroupItem>
+      <div>
+        <p>{reply.description}</p>
+      </div>
+    </ListGroupItem>
+  );
 }
 
 export default ReplyItem;
