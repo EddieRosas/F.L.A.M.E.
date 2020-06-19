2ade6941ffdd36f16e0e769bb9b69587f688be22
bcwan <bcwan8@gmail.com>
2020-06-19 14:56:23 -0700

    Set up ReplyIndex component

---
+++ frontend/src/components/reply/reply_index/reply_index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReplyItem from './reply_item';
+
+class ReplyIndex extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default ReplyIndex;
