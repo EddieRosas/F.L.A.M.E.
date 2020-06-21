a68632217227efc4fbbacfbf752f630508aea62e
bcwan <bcwan8@gmail.com>
2020-06-20 21:54:28 -0700

    Add reply details to state

--- frontend/src/components/reply/new_reply/new_reply.jsx
+++ frontend/src/components/reply/new_reply/new_reply.jsx
@@ -5,13 +5,15 @@ class NewReply extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      description: ""
+      username: this.props.currentUser.username,
+      postId: this.props.postId,
+      body: ""
     }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit() {
-
+    this.props.createReply(this.state);
   }
 
   render() {
