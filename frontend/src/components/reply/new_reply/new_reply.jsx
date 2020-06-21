29e8dfd504a8033ae6f0bbc682f43c73d3911ab7
bcwan <bcwan8@gmail.com>
2020-06-20 19:53:39 -0700

    Create skeleton of new reply

---
+++ frontend/src/components/reply/new_reply/new_reply.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+class NewReply extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+
+  }
+
+  render() {
+    return null;
+  }
+}
