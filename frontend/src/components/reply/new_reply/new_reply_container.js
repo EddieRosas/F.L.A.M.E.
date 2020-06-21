16a8999df5f551212603990e50a19b218448338e
bcwan <bcwan8@gmail.com>
2020-06-20 18:20:36 -0700

    Add new reply container

--- frontend/src/components/reply/new_reply/new_reply_container.js
+++ frontend/src/components/reply/new_reply/new_reply_container.js
@@ -2,3 +2,13 @@ import { connect } from 'react-redux';
 import NewReply from './new_reply';
 import { createReply } from '../../../actions/reply_actions';
 
+const mSTP = (state, ownProps) => ({
+  currentUser: state.session.user,
+  postId: ownProps.postId
+});
+
+const mDTP = (dispatch) => ({
+  createReply: (reply) => dispatch(createReply(reply))
+});
+
+export default connect(mSTP, mDTP)(NewReply);
