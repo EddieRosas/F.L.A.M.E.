5349f41f55fb77267ece939bef6c331361607f24
bcwan <bcwan8@gmail.com>
2020-08-10 15:25:07 -0700

    Load index.html in order to receive access to React Router and React components

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./reset.css";
@@ -21,6 +21,7 @@ const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
+      <Route exact path="/" component={MainPage} />
       <ProtectedRoute exact path="/budget" component={BudgetContainer} />
       <ProtectedRoute exact path="/profile" component={ProfileContainer} />
       <ProtectedRoute
@@ -33,7 +34,7 @@ const App = () => (
 
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <AuthRoute exact path="/" component={MainPage} />
+      {/* <AuthRoute exact path="/" component={MainPage} /> */}
     </Switch>
   </div>
 );
